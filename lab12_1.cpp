1423643d18efbbb1f4b42169fb6e6244c8ca5134
Porsche <peerawit1142@gmail.com>
2023-12-26 14:32:51 +0700

    lab12_1Complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x){
+    if(x>1){
+        int i=1;
+        return fibonacci(x-i) + fibonacci(x-(i+1));
+        i++;
+    }else return x;
 }
